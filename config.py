688d5827bcbf25ed516a59318a9535ffdcdb2b4b
Rachel Kennelly <rekennelly@gmail.com>
2022-03-01 23:17:01 -0500

    config.py reformatting

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
 BASE_URL = "https://api.discogs.com/"
 GRADES_DICT = {"M": "Mint (M)", "NM": "Near Mint (NM or M-)", "VG+": "Very Good Plus (VG+)", "VG": "Very Good (VG)", "G+": "Good Plus (G+)", "G": "Good (G)", "F": "Fair (F)", "P": "Poor (P)"}
-
-
-
-
